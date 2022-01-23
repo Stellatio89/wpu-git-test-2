b2ac727a5383052dcac3f2ca5702472cf14f7be3
Stella Tio <tiostella288@gmail.com>
2022-01-23 13:05:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, aperiam voluptates quisquam error modi unde similique quam eum beatae veniam ullam necessitatibus nam, dolores, mollitia sunt porro architecto vitae commodi!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua </li>
+    </ul>
 </body>
 </html>
